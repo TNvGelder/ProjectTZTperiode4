635a701c3afbbf36abc57552631aa316a8581d0b
TNvGelder <TNvGelder@gmail.com>
2015-04-28 14:49:29 +0200

    code voor database

--- eigendatabasetest/beginQueries.sql
+++ eigendatabasetest/beginQueries.sql
@@ -2,3 +2,4 @@ INSERT INTO stakeholdertype VALUES(2, 'geverifieerd');
 INSERT INTO locatie VALUES(1, 'Stationsweg', '5B', 'Zwolle', '1234AB');
 INSERT into stakeholder VALUES(1, 2, 'Test', 'Jansen', 'Test@gmail.com', 0636546686, 'link naar IDkaartscan', 'link naar OVscan', 7.76, NULL, 'hashwachtwoord hier', 1, NULL, 'rekingnr');
 INSERT INTO verzendorder VALUES(1, 1, now());
+
