15c0e96a7a4ac3ef7fb9819c545b7bc63863bb50
TNvGelder <TNvGelder@gmail.com>
2015-05-09 15:02:43 +0200

    Base en ContactBase aangemaakt.

--- JAVA/src/TZTBackOffice/ContactBase.java
+++ JAVA/src/TZTBackOffice/ContactBase.java
@@ -9,6 +9,23 @@ package TZTBackOffice;
  *
  * @author Twan
  */
-public class ContactBase {
+public class ContactBase extends Base<Contact>{
+
+    
+    public ContactBase(){
+        super();
+        
+    }
+    
+    //Update het contact in de database, zodat het overeenkomt met de gegevens van Contact c.
+    public void wijzigContact(Contact c){
+        
+    }
+    
+    @Override
+    public void haalDataOp() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
