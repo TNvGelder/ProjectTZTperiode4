cee49ce38ba0d68958a6901f6c0833f0fcb772ff
Jasper F <jasperfolkertsma@gmail.com>
2015-05-13 10:52:17 +0200

    PakketOverzichtPanel overzicht nu met combobox en tabel - sorteren nog toevoegen + onbewerkbaar maken ButtonEditor/Renderer voor button in tabel Hoofdscherm refresh knop toevoegen - layout nog nodig

--- JAVA/src/TZTBackOffice/HoofdScherm.java
+++ JAVA/src/TZTBackOffice/HoofdScherm.java
@@ -6,9 +6,12 @@
 package TZTBackOffice;
 
 import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -17,25 +20,36 @@ import javax.swing.JTabbedPane;
  *
  * @author Twan
  */
-public class HoofdScherm extends JFrame {
+public class HoofdScherm extends JFrame implements ActionListener {
 
     public HoofdScherm() {
         super("BackOffice Applicatie");
+        setLayout(new FlowLayout());
         setSize(1280, 680);
 
         JTabbedPane tabbedPane = new JTabbedPane();
         ContactOverzichtPanel contactPanel = new ContactOverzichtPanel();
         PakketOverzichtPanel pakketPanel = new PakketOverzichtPanel();
+
         JComponent panel1 = new JPanel();
         JComponent panel4 = new JPanel();
         tabbedPane.addTab("Problemen", panel1);
         tabbedPane.addTab("Pakketten", pakketPanel);
         tabbedPane.addTab("Klanten & Koeriers", contactPanel);
         tabbedPane.addTab("Uitbetalingsverzoeken", panel4);
-        this.add(tabbedPane);
-        setVisible(true);
-        
 
+        this.add(tabbedPane);
+        JButton jbRefresh = new JButton("Refresh");
+        add(jbRefresh);
+        jbRefresh.addActionListener(this);
+
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
--- JAVA/src/TZTBackOffice/PakketOverzichtPanel.java
+++ JAVA/src/TZTBackOffice/PakketOverzichtPanel.java
@@ -56,19 +56,72 @@ public class PakketOverzichtPanel extends JPanel implements ItemListener {
 
         //Create the "cards".
         JPanel card1 = new JPanel();
-        card1.add(new JButton("Button 1"));
-        card1.add(new JButton("Button 2"));
-        card1.add(new JButton("Button 3"));
+
+        DefaultTableModel dmAangemeld = new DefaultTableModel();
+
+        dmAangemeld.setDataVector(new Object[][]{{"77777", "16:44 PM", "20:04 PM", "24/4/15", "Aangemeld BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"43254", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"}},
+                new Object[]{"Pakket nr", "Aanmeldtijd", "Aflevertijd", "Datum", "Organisatie", "Formaat", "Gewicht", "Betaald", "Details"});
+
+        JTable tableAangemeld = new JTable(dmAangemeld);
+        tableAangemeld.getColumn("Details").setCellRenderer(new ButtonRenderer());
+        tableAangemeld.getColumn("Details").setCellEditor(new ButtonEditor(new JCheckBox()));
+        tableAangemeld.setPreferredScrollableViewportSize(new Dimension(800, 140));
+        JScrollPane scrollAangemeld = new JScrollPane(tableAangemeld);
+        card1.add(scrollAangemeld);
 
         JPanel card2 = new JPanel();
-        card2.add(new JTextField("TextField", 20));
+        DefaultTableModel dmVerzonden = new DefaultTableModel();
+
+        dmVerzonden.setDataVector(new Object[][]{{"77777", "16:44 PM", "20:04 PM", "24/4/15", "Verzonden BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"43254", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"}},
+                new Object[]{"Pakket nr", "Aanmeldtijd", "Aflevertijd", "Datum", "Organisatie", "Formaat", "Gewicht", "Betaald", "Details"});
+
+        JTable tableVerzonden = new JTable(dmVerzonden);
+        tableVerzonden.getColumn("Details").setCellRenderer(new ButtonRenderer());
+        tableVerzonden.getColumn("Details").setCellEditor(new ButtonEditor(new JCheckBox()));
+        tableVerzonden.setPreferredScrollableViewportSize(new Dimension(800, 140));
+        JScrollPane scrollVerzonden = new JScrollPane(tableVerzonden);
+        card2.add(scrollVerzonden);
+
+        JPanel card3 = new JPanel();
+        DefaultTableModel dmGearriveerd = new DefaultTableModel();
+
+        dmGearriveerd.setDataVector(new Object[][]{{"77777", "16:44 PM", "20:04 PM", "24/4/15", "Gearriveerd BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"55555", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"},
+        {"43254", "16:44 PM", "20:04 PM", "24/4/15", "Esyst BV", "30x40x10", "476 g", "Betaald", "Meer info"}},
+                new Object[]{"Pakket nr", "Aanmeldtijd", "Aflevertijd", "Datum", "Organisatie", "Formaat", "Gewicht", "Betaald", "Details"});
+
+        JTable tableGearriveerd = new JTable(dmGearriveerd);
+        tableGearriveerd.getColumn("Details").setCellRenderer(new ButtonRenderer());
+        tableGearriveerd.getColumn("Details").setCellEditor(new ButtonEditor(new JCheckBox()));
+        tableGearriveerd.setPreferredScrollableViewportSize(new Dimension(800, 140));
+        JScrollPane scrollGearriveerd = new JScrollPane(tableGearriveerd);
+        card3.add(scrollGearriveerd);
 
         //Create the panel that contains the "cards".
         cards = new JPanel(new CardLayout());
         cards.add(card1, Aangemelde);
         cards.add(card2, Verzonden);
+        cards.add(card3, Gearriveerde);
         add(cb);
         add(cards, BorderLayout.CENTER);
+
+        setVisible(true);
 
 //        pane.add(comboBoxPane, BorderLayout.PAGE_START);
 //        pane.add(cards, BorderLayout.CENTER);
@@ -86,14 +139,12 @@ public class PakketOverzichtPanel extends JPanel implements ItemListener {
         //Create the "cards".
         JPanel card1 = new JPanel();
         card1.add(new JButton("Button 1"));
-        card1.add(new JButton("Button 2"));
-        card1.add(new JButton("Button 3"));
 
         JPanel card2 = new JPanel();
         card2.add(new JTextField("TextField", 20));
 
         JPanel card3 = new JPanel();
-        card3.add(new JTextField("3", 20));
+        card3.add(new JTextField("swag", 5));
 
         //Create the panel that contains the "cards".
         cards = new JPanel(new CardLayout());
@@ -114,43 +165,43 @@ public class PakketOverzichtPanel extends JPanel implements ItemListener {
      * Create the GUI and show it. For thread safety, this method should be
      * invoked from the event dispatch thread.
      */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Buttons");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Create and set up the content pane.
-        PakketOverzichtPanel demo = new PakketOverzichtPanel();
-        demo.addComponentToPane(frame.getContentPane());
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+//    private static void createAndShowGUI() {
+//        //Create and set up the window.
+//        JFrame frame = new JFrame("Buttons");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        //Create and set up the content pane.
+//        PakketOverzichtPanel demo = new PakketOverzichtPanel();
+//        demo.addComponentToPane(frame.getContentPane());
+//
+//        //Display the window.
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
+//
+//    public static void main(String[] args) {
+//        /* Use an appropriate Look and Feel */
+//        try {
+//            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+//            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+//        } catch (UnsupportedLookAndFeelException ex) {
+//            ex.printStackTrace();
+//        } catch (IllegalAccessException ex) {
+//            ex.printStackTrace();
+//        } catch (InstantiationException ex) {
+//            ex.printStackTrace();
+//        } catch (ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        }
+//        /* Turn off metal's use of bold fonts */
+//        UIManager.put("swing.boldMetal", Boolean.FALSE);
+//
+//        //Schedule a job for the event dispatch thread:
+//        //creating and showing this application's GUI.
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                createAndShowGUI();
+//            }
+//        });
+//    }
 }
