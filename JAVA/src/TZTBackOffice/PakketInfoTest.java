8a7f8b57e64e5790908705ceff52bc774c38c9fe
TNvGelder <TNvGelder@gmail.com>
2015-05-18 16:45:25 +0200

    pakketinfotestupdate

--- JAVA/src/TZTBackOffice/PakketInfoTest.java
+++ JAVA/src/TZTBackOffice/PakketInfoTest.java
@@ -2,6 +2,7 @@ package TZTBackOffice;
 
 import java.time.LocalDate;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 
 /**
@@ -15,9 +16,14 @@ public class PakketInfoTest {
         AccountHouder klant = new AccountHouder("Piet", "gebruiker", "Piet@gmail.com", "03342023423", 1, "Pietersen", locatie);
         LocalDate localDateTime = LocalDate.now();
         VerzendOrder verzendOrder = new VerzendOrder(1, klant, new Timestamp(1));
-        Pakket p = new Pakket(1, 200, "30x20x15", verzendOrder, "breekbaar", "onderweg");
+        //Pakket p = new Pakket(1, 200, "30x20x15", verzendOrder, "breekbaar", "onderweg");
+        DatabaseManager databaseManager = new DatabaseManager();
+        ArrayList<Pakket> pakketten = databaseManager.getPakketten();
+        Pakket pakket = pakketten.get(1);
+        System.out.println("\n"+ pakket);
         
-        PakketInfoDialoog pakketInfo = new PakketInfoDialoog(p);
+        
+        PakketInfoDialoog pakketInfo = new PakketInfoDialoog(pakket);
         JFrame testScherm = new JFrame();
         
         
